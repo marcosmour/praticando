f4c5f5ac1a235803ecc8453b8eea3ff293fe1431
Marcos Moura <marcosmoura201@hotmail.com>
2022-06-21 14:21:33 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/mmpcoder/praticando/domain/Categoria.java
+++ src/main/java/com/mmpcoder/praticando/domain/Categoria.java
@@ -3,17 +3,17 @@ package com.mmpcoder.praticando.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-//@Entity
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	//@Id
-	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
